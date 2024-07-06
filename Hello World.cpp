68243b6ae43293b47da884089ff5376d2f94919f
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 16:54:50 +0530

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+
+//Using cout for console output
+//Returning 0 to indicate successful program execution

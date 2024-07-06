ccedf4432b91a7a13505338a25950a41f653c997
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 17:12:22 +0530

    Create Ques.) Write a program that reads two characters from the user and checks if they are the same.cpp

---
+++ Ques.) Write a program that reads two characters from the user and checks if they are the same.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char ch1, ch2;
+
+  cout << "Enter two characters: ";
+  cin >> ch1 >> ch2;
+
+  if (ch1 == ch2) {
+    cout << "The characters are the same." << endl;
+  } else {
+    cout << "The characters are different." << endl;
+  }
+
+  return 0;
+}
+
+
+//we have used if else statement here assume like if this is there then print this else print this.... i hope you understood...//

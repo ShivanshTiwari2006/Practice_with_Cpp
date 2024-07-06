f21bd471e4a4cc088788d1d05f2be8e22267bdf1
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 16:57:58 +0530

    Create Variables and Data Types.cpp

---
+++ Variables and Data Types.cpp
@@ -0,0 +1,13 @@
+int age;  // Declares an integer variable named age
+double pi = 3.14159;  // Declares a double variable named pi and initializes it with a value
+char initial = 'S';  // Declares a char variable named initial and initializes it
+bool isSunny = true;  // Declares a bool variable named isSunny and initializes it
+
+
+//here all the int, double, char, bool (boolen) are the variables.
+
+int sum = age + 10;  // Expression calculates sum using variables and operators
+
+
+//in this above it will calculate the age +10 as commanded above when user input the age it will addup 10 into the age and print the value.
+

b7ed820e52b160659ef16bb35426da16fad55e00
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 16:59:50 +0530

    Create input and output.cpp

---
+++ input and output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num;
+
+  cout << "Enter a number: ";
+  cin >> num;  // Reads user input and stores it in num
+
+  cout << "You entered: " << num << endl;
+
+  return 0;
+}
+
+
+
+
+//cin it allows user to input anything 

0e04e72d1c7b8b952ddb52717d42f940b525a4b4
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 17:09:27 +0530

    Create Quest.) Write a program that prompts the user for their age and then prints it doubled.cpp

---
+++ Quest.) Write a program that prompts the user for their age and then prints it doubled.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age;
+
+  cout << "Enter your age: ";
+  cin >> age;
+
+  cout << "Your age doubled is: " << age * 2 << endl;
+
+  return 0;
+}

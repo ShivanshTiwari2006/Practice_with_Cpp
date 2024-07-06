f1bf6bd6f21242c36d436c46c7474c35fd8434ed
Shivansh Tiwari <168632802+ShivanshTiwari2006@users.noreply.github.com>
2024-07-06 17:07:27 +0530

    Create Declare variables to store your name (string), age (int), and height (double).cpp

---
+++ Declare variables to store your name (string), age (int), and height (double).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    double height;
+
+    cout << "Plz Enter Your: " ;
+    cin >> name ;
+
+    cout << "plz enter your age: " ;
+    cin >> age ;
+
+
+    cout << "your name is : "<< name << endl;
+    cout << "your age is : "<< age << endl;
+    
+    return 0;
+}
+
+
+//see it was easy!!
